ba23629d2bd2d3168421514385509268f696cf3f
gnuhub <gnuhub@qq.com>
2023-12-13 17:59:27 +0800

    [just fix it]

--- ci/1020-docker-build/build.sh
+++ ci/1020-docker-build/build.sh
@@ -10,8 +10,6 @@ apt install multistrap -y
 
 multistrap -f beige.multistrap || true
 
-cp deepin.gpg /beige-rootfs/etc/apt/trusted.gpg.d
-
 multistrap -f beige.multistrap
 
 cp sources.list /beige-rootfs/etc/apt/sources.list
