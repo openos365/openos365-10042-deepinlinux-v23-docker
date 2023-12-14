965d6525f66f94b152a6a98b75f4a63316feac60
gnuhub <gnuhub@qq.com>
2023-12-15 04:09:55 +0800

    [just fix it]

--- ci/1021-docker-build-linglong/1.ci.run.sh
+++ ci/1021-docker-build-linglong/1.ci.run.sh
@@ -10,7 +10,7 @@ sudo apt install ./*.deb
 cd $CMD_PATH
 mkdir -p $HOME/.cache/linglong-builder/repo
 
-ostree -v --repo=$HOME/.cache/linglong-builder/repo init --mode=bare-user-only
+ostree -v --repo=$HOME/.cache/linglong-builder/repo remote delete repo
 
 ostree -v --repo=$HOME/.cache/linglong-builder/repo remote add --no-gpg-verify repo https://mirror-repo-linglong.deepin.com/repos/repo
 
